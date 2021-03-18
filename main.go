0492382c3bbdf47abdd99b8324543a44b0fc6c3a
Alec Smrekar <alec.smrekar@gmail.com>
2021-03-18 07:51:45 +0100

    Implemented concurrency safe client map

--- main.go
+++ main.go
@@ -4,9 +4,65 @@ import (
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
+	"sync"
 )
 
-var clients = make(map[*websocket.Conn]bool)
+// Concurrency safe map of clients
+// http://dnaeon.github.io/concurrent-maps-and-slices-in-go/
+type ClientsMap struct {
+	sync.RWMutex
+	items map[*websocket.Conn]interface{}
+}
+
+// Concurrent map of clients
+type ClientsMapItem struct {
+	Key   *websocket.Conn
+	Value interface{}
+}
+
+// Sets a key in the concurrent map of clients
+func (clients *ClientsMap) Set(connectionKey *websocket.Conn, value interface{}) {
+	clients.Lock()
+	defer clients.Unlock()
+	clients.items[connectionKey] = value
+}
+
+// Sets a key in the concurrent map of clients
+func (clients *ClientsMap) Delete (connectionKey *websocket.Conn) {
+	clients.Lock()
+	defer clients.Unlock()
+	delete(clients.items, connectionKey)
+}
+
+// Gets a key from the concurrent map  of clients
+func (clients *ClientsMap) Get(connectionKey *websocket.Conn) (interface{}, bool) {
+	clients.Lock()
+	defer clients.Unlock()
+	value, ok := clients.items[connectionKey]
+	return value, ok
+}
+
+// Iterates over the items in a concurrent map
+// Each item is sent over a channel, so that
+// we can iterate over the map using the builtin range keyword
+func (clients *ClientsMap) Iter() <-chan ClientsMapItem {
+
+	c := make(chan ClientsMapItem)
+
+	f := func() {
+		clients.Lock()
+		defer clients.Unlock()
+
+		for k, v := range clients.items {
+			c <- ClientsMapItem{k, v}
+		}
+		close(c)
+	}
+	go f()
+	return c
+}
+
+var clients_map = ClientsMap{items: make(map[*websocket.Conn]interface{})}
 var broadcast = make(chan Message)
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
@@ -26,7 +82,8 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 	}
 	// ensure connection close when function returns
 	defer ws.Close()
-	clients[ws] = true
+	clients_map.Set(ws, true)
+
 
 	for {
 		var msg Message
@@ -34,7 +91,7 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 		err := ws.ReadJSON(&msg)
 		if err != nil {
 			log.Printf("error: %v", err)
-			delete(clients, ws)
+			clients_map.Delete(ws)
 			break
 		}
 		// send the new message to the broadcast channel
@@ -47,12 +104,13 @@ func handleMessages() {
 		// grab next message from the broadcast channel
 		msg := <-broadcast
 		// send it out to every client that is currently connected
-		for client := range clients {
+		for KeyValPair := range clients_map.Iter() {
+			client := KeyValPair.Key
 			err := client.WriteJSON(msg)
 			if err != nil {
 				log.Printf("error: %v", err)
 				client.Close()
-				delete(clients, client)
+				clients_map.Delete(client)
 			}
 		}
 	}
@@ -64,7 +122,9 @@ func main() {
 	http.Handle("/", fs)
 
 	http.HandleFunc("/ws", handleConnections)
-	go handleMessages()
+	for i := 0; i < 2; i++ {
+		go handleMessages()
+	}
 
 	log.Println("http server started on :8000")
 	err := http.ListenAndServe(":8000", nil)
